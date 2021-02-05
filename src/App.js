bcc0882fd0d17e78b718b23671869ec1e5af1b12
Steven Victor Mycynek <smycynek@users.noreply.github.com>
2021-02-05 11:50:37 -0500

    Merge in url parse changes (#1)
    * url parse prototype 2
    
    * fix link prefix
    
    * use hashrouter
    
    * paste fix
    
    * read only fix
    
    Co-authored-by: Steven Mycynek <smycynek@buildingengines.com>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
@@ -16,7 +18,7 @@ import 'react-router-tabs/styles/react-router-tabs.css';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import {
-  BrowserRouter as Router, Route, Switch,
+  HashRouter, Route, Switch,
 } from 'react-router-dom';
 
 import { NavTab } from 'react-router-tabs';
@@ -28,7 +30,7 @@ import { Footer } from './footer';
 import infoButton from './images/information-button.svg';
 import terrain from './images/terrain.svg';
 
-function App({ match }) {
+function App() {
   const [showAbout, setShowAbout] = useState(false);
   const [squareTextures, setSquareTextures] = useState(true);
 
@@ -40,8 +42,10 @@ function App({ match }) {
   // eslint-disable-next-line no-unused-vars
   const handleToggleTextures = (e) => {
     document.body.classList.toggle('tbg-textured');
-    document.getElementById('btn-answer').classList.toggle('styled-button-textured');
-    document.getElementById('btn-next').classList.toggle('styled-button-textured');
+    const buttons = document.getElementsByClassName('styled-button');
+    for (let idx = 0; idx < buttons.length; idx++) {
+      buttons[idx].classList.toggle('styled-button-textured');
+    }
     setSquareTextures(!squareTextures);
   };
 
@@ -53,18 +57,18 @@ function App({ match }) {
   return (
     <>
       <h1 className="banner">Chess Puzzles</h1>
-      <Router>
-        <NavTab exact strict={false} to="/chess">View</NavTab>
-        <NavTab exact strict={false} to="/chess/create">Create</NavTab>
+      <HashRouter basename="/chess">
+        <NavTab exact strict={false} to="/">Samples</NavTab>
+        <NavTab exact strict={false} to="/create">View/Create</NavTab>
         <Switch>
-          <Route exact strict={false} path="/chess">
+          <Route exact strict={false} path="/">
             <StaticPuzzles squareTextures={squareTextures} />
           </Route>
-          <Route exact strict={false} path="/chess/create">
+          <Route exact strict={false} path="/create">
             <CreatePuzzles squareTextures={squareTextures} />
           </Route>
         </Switch>
-      </Router>
+      </HashRouter>
       <div className="row expanded caption">
         <img style={{ width: '1.5em', height: '1.5em' }} alt="info" src={infoButton} onClick={handleShowAboutClick} />
         <img style={{ marginLeft: '0.5em', width: '1.5em', height: '1.5em' }} alt="info" src={terrain} onClick={handleToggleTextures} />
